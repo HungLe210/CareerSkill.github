4985b0dce12a62d31d97b037722997accb00df5a
UIT-20521362 <20521362@gm.uit.edu.vn>
2021-12-19 21:02:31 +0700

    add remove_pause

--- KNNN.cpp
+++ KNNN.cpp
@@ -255,3 +255,28 @@ void pause_menu() {// to print pause menu
 		cout << "PRESS 'P' TO CONTINUE";
 	}
 }
+void remove_pause() {// to remove th eprinted pausee menu
+	if (colour) {
+		setcursor(1, (width - 5) / 2 + 2);
+		setConsoleColour(consolebackground::BLACK);
+		for (int i = 0; i < 5; i++) {
+			setcursor(1, (width - 1) / 2 + i);
+			for (int j = 0; j < lenght; j++)					cout << " ";
+		}
+	}
+	else {
+		setcursor((lenght - 9) / 2, (width - 1) / 2 + 1);
+		cout << "         ";
+		setcursor((lenght - 19) / 2, (width - 1) / 2 + 3);
+		cout << "                     ";
+	}
+	setcursor(food_y + 1, food_x + 2);//printing food again, if food is below pause menu, then it would have become blank
+	if (colour) {
+		setConsoleColour(consolebackground::GREEN);
+		cout << " ";
+	}
+	else {
+		setConsoleColour(consoleforeground::GREEN);
+		cout << "o";
+	}
+}
